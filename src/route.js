75e0de87c36b4fde5dee8db7453d9d3239e32fcd
Luis Felipe <lipe.lftn@gmail.com>
2021-06-25 17:30:50 -0300

    importando data base

--- src/route.js
+++ src/route.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const questionController = require('./controllers/questionController')
+const roomController = require('./controllers/roomController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render('index'));
-route.get('/room', (req, res) => res.render('room'));
-route.get('/create-room', (req, res) => res.render('create-room'));
+route.get('/', (req, res) => res.render('index', {page: 'enter-room'}));
+route.get('/room/:room', (req, res) => res.render('room'));
+route.get('/create-room', (req, res) => res.render('index', {page: 'create-room'}));
 
 // Formato que o formulario de dentro da modal tem que passar a informação
-route.post('/room/:room/:question/:action', questionController.index);
+route.post('/question/:room/:question/:action', questionController.index);
+route.post('/create-pass', roomController.create)
 
 module.exports = route;
